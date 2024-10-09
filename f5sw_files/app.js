d85bdde6a2557bb0e74781644e4601d33268f3d8
cmteaoliveira <cmteaoliveira@gmail.com>
2024-10-09 19:49:23 -0300

    .

--- f5sw_files/app.js
+++ f5sw_files/app.js
@@ -9,7 +9,7 @@ new Swiper("#swiper-2", {
         clickable: true,
         renderBullet: function (index, className) {
         return `<div class=${className}>
-            
+            <span class="number"></span>
             <span class="line"></span>
             </div>`;
         }
