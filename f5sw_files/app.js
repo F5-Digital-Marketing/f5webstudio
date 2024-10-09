0d3109be2dba4aa4926cfeb2d677c9d61a063dc5
cmteaoliveira <cmteaoliveira@gmail.com>
2024-10-09 12:21:55 -0300

    .

--- f5sw_files/app.js
+++ f5sw_files/app.js
@@ -1,17 +1,3 @@
-new Swiper("#swiper-1", {
-    effect: "fade",
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: "#swiper-1 .swiper-pagination",
-        clickable: true,
-    },
-    lazyLoading: true,
-    loop: true
-});
-
 new Swiper("#swiper-2", {
     slidesPerView: 1,
     centeredSlides: true,
@@ -37,7 +23,7 @@ new Swiper("#swiper-2", {
     },
     breakpoints: {
         800: {
-            slidesPerView: 1.5
+            slidesPerView: 2.5
         },
         1400: {
             slidesPerView: 3
