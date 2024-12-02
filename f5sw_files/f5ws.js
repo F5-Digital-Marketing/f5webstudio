2eaf5f1cd986b07a18d3e154da647d17ddb52aa1
cmteaoliveira <cmteaoliveira@gmail.com>
2024-12-02 16:22:35 -0300

    .

--- f5sw_files/f5ws.js
+++ f5sw_files/f5ws.js
@@ -55585,36 +55585,3 @@ Webflow.require('commerce') && Webflow.require('commerce').init({
     apiUrl: "https://render.webflow.com"
 });
 
-
-new Swiper("#swiper-2", {
-    slidesPerView: 1,
-    centeredSlides: true,
-    spaceBetween: 24,
-    pagination: {
-        el: ("#swiper-2 .swiper-custom-pagination"),
-        clickable: true,
-        renderBullet: function(index, className) {
-            return `<div class=${className}>
-          <span class="number"></span>
-          <span class="line"></span>
-          </div>`;
-        }
-    },
-    lazyLoading: true,
-    loop: true,
-    keyboard: {
-        enabled: true,
-    },
-    navigation: {
-        nextEl: "#nav-right",
-        prevEl: "#nav-left"
-    },
-    breakpoints: {
-        800: {
-            slidesPerView: 1.5
-        },
-        1400: {
-            slidesPerView: 3
-        }
-    }
-});
